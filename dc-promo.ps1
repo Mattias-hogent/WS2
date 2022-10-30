c771ccd289fcf61ed7930b42774d184aa482294d
Mattias Verscheure <mattias.verscheure@student.hogent.be>
2022-10-30 17:16:09 +0100

    Update dc-promo.ps1

--- dc-promo.ps1
+++ dc-promo.ps1
@@ -1,1 +1,14 @@
-﻿Import-Module ADDSDeployment
+﻿Install-windowsfeature -name AD-Domain-Services
+Import-Module ADDSDeployment
+Install-ADDSForest `
+-CreateDnsDelegation:$false `
+-DatabasePath "C:\Windows\NTDS" `
+-DomainMode "WinThreshold" `
+-DomainName "WS2-2223-mattias.hogent" `
+-DomainNetbiosName "WS2-2223-MATTIAS" `
+-ForestMode "WinThreshold" `
+-InstallDns:$true `
+-LogPath "C:\Windows\NTDS" `
+-NoRebootOnCompletion:$false `
+-SysvolPath "C:\Windows\SYSVOL" `
+-Force:$true
