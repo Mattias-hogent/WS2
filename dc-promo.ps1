37610061fe90310bba9860c4e9317e061a356bcc
Mattias Verscheure <mattias.verscheure@student.hogent.be>
2022-10-30 16:59:07 +0100

    Update dc-promo.ps1

--- dc-promo.ps1
+++ dc-promo.ps1
@@ -1,17 +1,17 @@
 ﻿#
 # Windows PowerShell script for AD DS Deployment
 #
-
+#Install-windowsfeature -name AD-Domain-Services
 Import-Module ADDSDeployment
-Install-ADDSForest `
--CreateDnsDelegation:$false `
--DatabasePath "C:\Windows\NTDS" `
--DomainMode "WinThreshold" `
--DomainName "WS2-2223-mattias.hogent" `
--DomainNetbiosName "WS2-2223-MATTIAS" `
--ForestMode "WinThreshold" `
--InstallDns:$true `
--LogPath "C:\Windows\NTDS" `
--NoRebootOnCompletion:$false `
--SysvolPath "C:\Windows\SYSVOL" `
--Force:$true
+#Install-ADDSForest `
+#-CreateDnsDelegation:$false `
+#-DatabasePath "C:\Windows\NTDS" `
+#-DomainMode "WinThreshold" `
+#-DomainName "WS2-2223-mattias.hogent" `
+#-DomainNetbiosName "WS2-2223-MATTIAS" `
+#-ForestMode "WinThreshold" `
+#-InstallDns:$true `
+#-LogPath "C:\Windows\NTDS" `
+#-NoRebootOnCompletion:$false `
+#-SysvolPath "C:\Windows\SYSVOL" `
+#-Force:$true
