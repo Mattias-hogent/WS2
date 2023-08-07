d369132a55fe8540022cad82b732d2a21972d0c5
Mattias-hogent <mattias.verscheure@student.hogent.be>
2023-08-07 17:52:07 +0200

    Update installatie-script.ps1

--- installatie-script.ps1
+++ installatie-script.ps1
@@ -2,13 +2,12 @@ Invoke-Expression "& { $(Invoke-RestMethod https://aka.ms/install-powershell.ps1
 
 Set-ItemProperty -Path 'HKLM:\Software\Microsoft\Windows NT\CurrentVersion\WinLogon' -Name Shell -Value 'C:\Program Files\PowerShell\7\pwsh.exe'
 
-New-Item -Path C:\temp\log.txt -Force
-
-Write-Host "$([Environment]::GetFolderPath('Startup'))\ssh-setup.ps1"
+Write-Host $env:USERNAME
+Write-Host "$([System.Environment]::GetFolderPath('Startup'))\ssh-setup.ps1"
 Invoke-WebRequest -UseBasicParsing -Uri https://raw.githubusercontent.com/Mattias-hogent/WS2/main/ssh-setup.ps1 -OutFile "$([Environment]::GetFolderPath('Startup'))\ssh-setup.ps1"
 
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force -ErrorAction Continue
-$Error | Out-File c:\temp\log.txt -Append
+Start-Sleep(60)
 
 # installs updates
 Install-Module -Name PSWindowsUpdate -AcceptLicense -Force
