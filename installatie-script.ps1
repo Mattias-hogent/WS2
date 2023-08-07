a93f7e1a76bb9df3ecb3477bbf79bfa744cd8a27
Mattias-hogent <mattias.verscheure@student.hogent.be>
2023-08-08 01:07:49 +0200

    Update installatie-script.ps1

--- installatie-script.ps1
+++ installatie-script.ps1
@@ -2,8 +2,6 @@ Invoke-Expression "& { $(Invoke-RestMethod https://aka.ms/install-powershell.ps1
 
 Set-ItemProperty -Path 'HKLM:\Software\Microsoft\Windows NT\CurrentVersion\WinLogon' -Name Shell -Value 'C:\Program Files\PowerShell\7\pwsh.exe'
 
-Write-Host $env:USERNAME
-Write-Host "$([System.Environment]::GetFolderPath('Startup'))\ssh-setup.ps1"
 Invoke-WebRequest -UseBasicParsing -Uri https://raw.githubusercontent.com/Mattias-hogent/WS2/main/ssh-setup.ps1 -OutFile "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\ssh-setup.ps1"
 
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force -ErrorAction Continue
