88e2c3583246995cfa9f023b79af8bd86fe6d223
Mattias-hogent <mattias.verscheure@student.hogent.be>
2023-08-07 17:04:59 +0200

    Update installatie-script.ps1

--- installatie-script.ps1
+++ installatie-script.ps1
@@ -3,7 +3,7 @@ Invoke-Expression "& { $(Invoke-RestMethod https://aka.ms/install-powershell.ps1
 Set-ItemProperty -Path 'HKLM:\Software\Microsoft\Windows NT\CurrentVersion\WinLogon' -Name Shell -Value 'C:\Program Files\PowerShell\7\pwsh.exe'
 
 Invoke-WebRequest -UseBasicParsing -Uri https://raw.githubusercontent.com/Mattias-hogent/WS2/main/ssh-setup.ps1 -OutFile "$([Environment]::GetFolderPath('Startup'))\ssh-setup.ps1" -ErrorAction Continue
-Write-Error $Error
+$Error | Out-File c:\temp\log.txt -Append
 
 # installs updates
 Install-Module -Name PSWindowsUpdate
