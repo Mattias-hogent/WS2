d8deb79c4abdd4554683b8eac63436d0967c7003
Mattias-hogent <mattias.verscheure@student.hogent.be>
2023-08-09 15:28:11 +0200

    changed for pwsh7

--- update.ps1
+++ update.ps1
@@ -9,7 +9,12 @@ else {
 }
 
 function updateWindows {
-    $trigger = New-JobTrigger -AtLogOn -User SERVER\Admin
-    Register-ScheduledJob -Name "updateWindows" -FilePath "~\Documents\update.ps1" -Trigger $trigger
+    $action = New-ScheduledTaskAction -Execute "C:\Program Files\PowerShell\7\pwsh.exe" -Argument "-file ~\Documents\update.ps1"
+    $trigger = New-ScheduledTaskTrigger -AtLogon
+    $principal = "SERVER\Admin"
+    $task = New-ScheduledTask -Action $action -Principal $principal -Trigger $trigger
+    Register-ScheduledTask T1 -InputObject $task
     Get-WindowsUpdate -WindowsUpdate -AcceptAll -AutoReboot -Install
 }
+
+
