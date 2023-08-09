0365f3a44b985e1beeba8b37719bdc9511d19c7c
Mattias-hogent <mattias.verscheure@student.hogent.be>
2023-08-09 15:39:57 +0200

    Update installatie-script.ps1

--- installatie-script.ps1
+++ installatie-script.ps1
@@ -2,8 +2,6 @@ Invoke-Expression "& { $(Invoke-RestMethod https://aka.ms/install-powershell.ps1
 
 Set-ItemProperty -Path 'HKLM:\Software\Microsoft\Windows NT\CurrentVersion\WinLogon' -Name Shell -Value 'C:\Program Files\PowerShell\7\pwsh.exe'
 
-Start-Process 'C:\Program Files\PowerShell\7\pwsh.exe' -NoNewWindow
-
 Invoke-WebRequest -UseBasicParsing -Uri https://raw.githubusercontent.com/Mattias-hogent/WS2/main/update.ps1 -OutFile "~\Documents\update.ps1"
 Invoke-WebRequest -UseBasicParsing -Uri https://raw.githubusercontent.com/Mattias-hogent/WS2/main/ssh-setup.ps1 -OutFile "~\Documents\ssh-setup.ps1"
 
@@ -13,6 +11,6 @@ Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 Install-Module -Name PSWindowsUpdate -Force
 Import-Module -Name PSWindowsUpdate
 
-."~\Documents\update.ps1"
+Start-Process 'C:\Program Files\PowerShell\7\pwsh.exe' -ArgumentList "-file ~\Documents\update.ps1"
 #Get-WindowsUpdate -MicrosoftUpdate -AcceptAll -IgnoreReboot -Install
 #Get-WindowsUpdate -WindowsUpdate -AcceptAll -AutoReboot -Install
