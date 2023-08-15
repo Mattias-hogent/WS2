1ed9f204e814329b28b2c0b0fe51df7510e12bf3
Mattias-hogent <mattias.verscheure@student.hogent.be>
2023-08-15 15:04:52 +0200

    Delete update.ps1

--- update.ps1
+++
@@ -1,6 +0,0 @@
-
-function updateWindows {
-    $action = New-ScheduledTaskAction -Execute pwsh.exe -Argument "-File C:\Users\Admin\Documents\ssh-setup.ps1"
-    $trigger = New-ScheduledTaskTrigger -AtLogOn
-    Register-ScheduledTask SSH_setup -Action $action -Trigger $trigger
-}
